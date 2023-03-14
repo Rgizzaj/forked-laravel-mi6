c37f2009349b2e14088274726d3dfb53727bc5bf
Mmarkev <monika@data4you.cz>
2023-03-14 11:16:22 +0100

    axios get example

--- resources/js/front/StatusFilter.jsx
+++ resources/js/front/StatusFilter.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react"
+import axios from 'axios';
 
 export default function StatusFilter({ selectedStatus, setSelectedStatus}) {
     const [statuses, setStatuses] = useState([]);
 
     const loadStatuses = async () => {
-        let response = await fetch('/api/statuses')
-        let data = await response.json();
-
-        console.log(data);
-        setStatuses(data);
+        try {
+            let response = await axios.get('/api/statuses');
+            setStatuses(response.data);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     useEffect(() => {
